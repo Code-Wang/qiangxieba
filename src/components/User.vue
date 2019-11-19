a00f0b870b93ec8c31223744b96d29280a970208
wangyaobo <175357374@qq.com>
2019-11-19 15:32:41 +0800

    add user function

--- src/components/User.vue
+++ src/components/User.vue
@@ -4,8 +4,7 @@
     ref="multipleTable"
     :data="tableData"
     tooltip-effect="dark"
-    style="width: 100%"
-    @selection-change="handleSelectionChange">
+    style="width: 100%">
     <el-table-column
       type="selection"
       width="55">
@@ -31,24 +30,37 @@
       width="120">
       <template slot-scope="scope">
         <el-button
-          @click.native.prevent="deleteRow(scope.$index, tableData)"
+          @click.native.prevent="editUser(scope.$index, tableData)"
           type="text"
           size="small">
-          	移除
-        </el-button>
-        <el-button
-          @click.native.prevent="deleteRow(scope.$index, tableData)"
-          type="text"
-          size="small">
-          	查看
+          	编辑
         </el-button>
       </template>
     </el-table-column>
   </el-table>
   <div style="margin-top: 20px">
-    <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
-    <el-button @click="toggleSelection()">取消选择</el-button>
+    <el-button @click="dialogVisible = true">新增用户</el-button>
   </div>
+
+  <el-dialog title="新增用户" 
+    :visible.sync="dialogVisible">
+    <el-form :model="newUser" class="add-user" label-width="80px">
+      <el-form-item label="用户名称*">
+        <el-input v-model="newUser.username" placeholder="用户名称"></el-input>
+      </el-form-item>
+      <el-form-item label="真实姓名*">
+        <el-input v-model="newUser.truename" placeholder="真实姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码*">
+        <el-input v-model="newUser.password" placeholder="密码"></el-input>
+      </el-form-item>
+    </el-form>
+
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="sendAddUserMsg">确 定</el-button>
+    </div>
+  </el-dialog>
 </div>
 </template>
 
@@ -59,7 +71,14 @@
         tableData: [],
         multipleSelection: [],
         pageIndex: 0,
-        pangeCount: 10
+        pangeCount: 10,
+        dialogVisible: false,
+        newUser:{
+          username:'',
+          truename:'',
+          password:'',
+        },
+        xxx:"用户名"
       }
     },
 
@@ -81,33 +100,62 @@
     },
 
     methods: {
-      toggleSelection(rows) {
-        var params = new URLSearchParams();
-        params.append("id",1); 
-        params.append("webset", "huawei");
-        params.append("account","123"); 
-        params.append("password","testpassword");
-        params.append("accountname","ceshi_account"); 
-        params.append("telphone","17764987932");
-        params.append("address","拓东体育馆"); 
-        params.append("paytype","wechat");
-        params.append("payaccount","wechat_test"); 
-        params.append("defaultsize",42);
-
-        this.$axios({
-            method: 'post',
-            url: this.ServerAddress + 'updateaccount',
-            contentType: 'application/x-www-form-urlencoded',
-            data:params,  
-        }).then(function(response) {
-            alert(response.data.desc)
-            }.bind(this)).catch(function (error) { 
-                console.log(error);
-            })      
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
+    sendAddUserMsg: function() {
+      if (this.newUser.username == '' ||
+          this.newUser.truename == '' ||
+          this.newUser.password == '' ){
+        alert("请填写所有的必填项")
+        return
       }
-    }
+
+      var params = new URLSearchParams();
+      params.append("username", this.newUser.username); 
+      params.append("truename", this.newUser.truename);
+      params.append("password", this.newUser.password);
+
+      this.$axios({
+          method: 'post',
+          url: this.ServerAddress + 'updateuser',
+          contentType: 'application/x-www-form-urlencoded',
+          data:params,  
+      }).then(function(response) {
+          alert(response.data.desc)
+          }.bind(this)).catch(function (error) { 
+              console.log(error);
+          })      
+    },
+
+    sendAddUserMsgxxx: function() {
+      var params = new URLSearchParams();
+      params.append("id",2); 
+      params.append("webset", "huawei");
+      params.append("account","123"); 
+      params.append("password","testpassword");
+      params.append("accountname","ceshi_account"); 
+      params.append("telphone","17764987932");
+      params.append("address","拓东体育馆"); 
+      params.append("paytype","wechat");
+      params.append("payaccount","wechat_test"); 
+      params.append("defaultsize",42);
+
+      console.log(params)
+      console.log(params.getAll('id'))
+
+      this.$axios({
+          method: 'post',
+          url: this.ServerAddress + 'updateaccount',
+          contentType: 'application/x-www-form-urlencoded',
+          data:params,  
+      }).then(function(response) {
+          alert(response.data.desc)
+          }.bind(this)).catch(function (error) { 
+              console.log(error);
+          })      
+    },
+
+    AddNewUser: function() {
+      this.dialogVisible = true
+    }    
   }
+}
 </script>
